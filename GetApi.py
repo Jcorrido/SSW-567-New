4566ce33ea0e3f26cbd9893ac86765d0bbfe93dd
jcorrido <jcorrido@stevens.edu>
2022-10-12 18:41:52 -0400

    code analyzer changes

--- GetApi.py
+++ GetApi.py
@@ -1,14 +1,14 @@
-import requests
 import json
+import requests
 
 def getRepoCommits(userID):
     out = {}
-    response = requests.get("https://api.github.com/users/" + userID + "/repos")
+    response = requests.get("https://api.github.com/users/" + userID + "/repos", timeout=5)
     if response.status_code == 200:
         json_data = json.loads(response.text)
         for repo in json_data:
             repo_name = repo.get("name")
-            commit_response = requests.get("https://api.github.com/repos/" + userID + "/" + repo_name + "/commits")
+            commit_response = requests.get("https://api.github.com/repos/" + userID + "/" + repo_name + "/commits", timeout=5)
             if commit_response.status_code == 200:
                 commit_data = json.loads(commit_response.text)
                 out[repo_name] = len(commit_data)
--- TestGetApi.py
+++ TestGetApi.py
@@ -6,16 +6,17 @@ class TestApi(unittest.TestCase):
         self.assertNotEqual(len(getRepoCommits("richkempinski")), 0, "dictionary should not be empty")
 
     def testDataHasRepositortNames(self):
-        dict = getRepoCommits("richkempinski")
-        self.assertNotEqual(dict["hellogitworld"], None, "repository 'Complexity' should be in dictionary")
+        dict1 = getRepoCommits("richkempinski")
+        self.assertNotEqual(dict1["hellogitworld"], None, "repository 'hellogitworld' should be in dictionary")
 
-    def testCommitCountCorrect(self):
-        dict = getRepoCommits("richkempinski")
-        self.assertNotEqual(len(dict), 0, "dictionary should not be empty")
-        if len(dict) == 0:
+    def testCommitCount(self):
+        dict1 = getRepoCommits("richkempinski")
+        self.assertNotEqual(len(dict1), 0, "dictionary should not be empty")
+        if len(dict1) == 0:
             return
-        self.assertEqual(dict["hellogitworld"], 29, "repository 'hellogitworld' should have 29 commits")
+        self.assertEqual(dict1["hellogitworld"], 30, "repository 'hellogitworld' should have 30 commits")
 
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
+    
