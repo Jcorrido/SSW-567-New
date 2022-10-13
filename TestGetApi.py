0bbaecc91cc0087d97c1f398b92e9be9c2dc3e11
jcorrido <jcorrido@stevens.edu>
2022-10-12 22:06:44 -0400

    Mocking changes

--- TestGetApi.py
+++ TestGetApi.py
@@ -1,20 +1,34 @@
 import unittest
-from GetApi import getRepoCommits
+from unittest import mock
+from unittest.mock import Mock, patch
+import GetApi
 
 class TestApi(unittest.TestCase):
-    def testDataNotEmpty(self):
-        self.assertNotEqual(len(getRepoCommits("richkempinski")), 0, "dictionary should not be empty")
 
-    def testDataHasRepositortNames(self):
-        dict1 = getRepoCommits("richkempinski")
-        self.assertNotEqual(dict1["hellogitworld"], None, "repository 'hellogitworld' should be in dictionary")
+    @patch('GetApi.getRepoCommits')
+    def testDataNotEmpty(self, mock_commits):
+        dict = {'hellogitworld': 30}
+        mock_commits.return_value = dict
 
-    def testCommitCount(self):
-        dict1 = getRepoCommits("richkempinski")
-        self.assertNotEqual(len(dict1), 0, "dictionary should not be empty")
-        if len(dict1) == 0:
+        self.assertNotEqual(len(GetApi.getRepoCommits("richkempinski")), 0, "dictionary should not be empty")
+
+    @patch('GetApi.getRepoCommits')
+    def testDataHasRepositortNames(self, mock_commits):
+        dict = {'hellogitworld': 30}
+        mock_commits.return_value = dict
+
+        self.assertNotEqual(dict["hellogitworld"], None, "repository 'hellogitworld' should be in dictionary")
+
+    @patch('GetApi.getRepoCommits')
+    def testCommitCount(self, mock_commits):
+        dict = {'hellogitworld': 30}
+        mock_commits.return_value = dict
+
+        dict = GetApi.getRepoCommits("richkempinski")
+        self.assertNotEqual(len(dict), 0, "dictionary should not be empty")
+        if len(dict) == 0:
             return
-        self.assertEqual(dict1["hellogitworld"], 30, "repository 'hellogitworld' should have 30 commits")
+        self.assertEqual(dict["hellogitworld"], 30, "repository 'hellogitworld' should have 30 commits")
 
 if __name__ == '__main__':
     print('Running unit tests')
